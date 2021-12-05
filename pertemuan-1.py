4d06d06324294df99b5280f85715cdf5208fd40a
maria <89510108+hestydms@users.noreply.github.com>
2021-12-05 13:13:36 +0700

    pertemuan-1.py

---
+++ pertemuan-1.py
@@ -0,0 +1,54 @@
+# ini comment
+
+# # ini nama
+# print("Halo, saya maria")
+
+# # ini alamat saya
+# print("rumah saya di depok")
+
+# x = 5 # integer
+# y = "Halo, dunia" # string
+# z = 2.5 # float
+# a = True # boolean
+
+# type data
+
+# print(x)
+# print(y)
+# print(z)
+# print(a)
+
+# print(type(a))
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+
+# math operation
+# p = 2
+# q = 3
+# r = p + q
+# print (p+q)
+# print (r)
+
+# print(p ** q)
+# print (r)
+
+# castings
+
+# x = int(2.8) # x will be 2
+# y = float("4.2") # y will be 4.2
+# z = str(3.2) # x will be string
+
+# print(x)
+# print(y)
+# print(z)
+# # print(y+z)
+# print(x+y)
+# print(type(2*6/3))
+
+x = 5
+y = x
+z = 'y'
+
+print(z)
